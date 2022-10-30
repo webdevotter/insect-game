ff61937d8f2d7f9c9e5ba212c13d9b5aa9114e96
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-30 10:55:41 -0500

    behavior

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const screens = document.querySelector('.screen')
 
-screens[0].classList.add('up')
+const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+
+choose_insect_btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img')
+    const src = img.getAttribute('src')
+    const alt = img.getAttribute('alt')
+    selected_insect = {src, alt}
+    screens[1].classList.add('up')
+    setTimeout(createInsect, 1000)
+    startGame()
+  })
+})
